df05f195690200bebf3bd2b80fba0a087bed71b4
Rohit Gupta <rohitrgupta@gmail.com>
2024-03-23 11:00:10 +0000

    add structs

---
+++ structs/structs_test.go
@@ -0,0 +1,42 @@
+package main
+
+import "testing"
+
+func TestPerimeterVal(t *testing.T) {
+	got := PerimeterVal(10.0, 10.0)
+	want := 40.0
+
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
+
+func TestAreaVal(t *testing.T) {
+	got := AreaVal(12.0, 6.0)
+	want := 72.0
+
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
+
+
+func TestArea(t *testing.T) {
+
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{12, 6}, 72.0},
+		{Circle{10}, 314.1592653589793},
+		{Triangle{12, 6}, 36.0},
+	}
+
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %g want %g", got, tt.want)
+		}
+	}
+
+}
