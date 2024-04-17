001b0242bbc7a22589e4830b35fdd9d223561339
Rohit Gupta <rohitrgupta@gmail.com>
2024-04-17 17:12:03 +0000

    split arrays test

--- 008_array_sum/arrays_test.go
+++ 008_array_sum/arrays_test.go
@@ -27,27 +27,3 @@ func TestSum(t *testing.T) {
 	})
 }
 
-func areEq(a, b []int) bool {
-	if len(a) != len(b) {
-        return false
-    }
-    for i := range a {
-        if a[i] != b[i] {
-            return false
-        }
-    }
-    return true
-}
-
-func TestArrayDiff(t *testing.T) {
-	t.Run("difference of arrays", func(t *testing.T) {
-		input := []int{1, 2, 2, 2, 3}
-		other := []int{2}
-		want := []int{1, 3}
-		got := ArrayDiff(input, other)
-		if !areEq(got, want) {
-			t.Errorf("got %v want %v", got, want)
-		}
-	
-	})
-}
---
+++ 008_array_sum/go.mod
@@ -0,0 +1,3 @@
+module array_sum
+
+go 1.21.5
