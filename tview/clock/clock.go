a051b0a5d325609b05fe10ac154d3039840b9731
Rohit Gupta <rohitrgupta@gmail.com>
2024-09-03 09:58:49 +0530

    added clock

--- tview/clock/clock.go
+++ tview/clock/clock.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"time"
 
 	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
@@ -17,11 +17,31 @@ type Digit struct {
 const (
 	Zero digitValue = iota
 	One
+	Two
+	Three
+	Four
+	Five
+	Six
+	Seven
+	Eight
+	Nine
+	Dot
+	Colon
 )
 
-var text = map[digitValue][][]rune{
-	Zero: [][]rune{[]rune(`███`), []rune(`█ █`), []rune(`█ █`), []rune(`█ █`), []rune(`███`)},
-	One:  [][]rune{[]rune(`██ `), []rune(` █ `), []rune(` █ `), []rune(` █ `), []rune(`███`)},
+var text = map[digitValue][]string{
+	Zero:  {"███", "█ █", "█ █", "█ █", "███"},
+	One:   {"██ ", " █ ", " █ ", " █ ", "███"},
+	Two:   {"███", "  █", "███", "█  ", "███"},
+	Three: {"███", "  █", "███", "  █", "███"},
+	Four:  {"█ █", "█ █", "███", "  █", "  █"},
+	Five:  {"███", "█  ", "███", "  █", "███"},
+	Six:   {"███", "█  ", "███", "█ █", "███"},
+	Seven: {"███", "  █", "  █", "  █", "  █"},
+	Eight: {"███", "█ █", "███", "█ █", "███"},
+	Nine:  {"███", "█ █", "███", "  █", "███"},
+	Dot:   {"   ", "   ", "   ", "   ", "  █"},
+	Colon: {"   ", " █ ", "   ", " █ ", "   "},
 }
 
 // NewDigit returns a new digit.
@@ -29,7 +49,7 @@ func NewDigit() *Digit {
 	box := tview.NewBox().SetBorder(true)
 	return &Digit{
 		Box:   box,
-		value: []digitValue{Zero, One},
+		value: []digitValue{Zero, One, Two, Three, Four, Five, Six, Seven, Eight, Nine, Dot, Colon},
 	}
 }
 
@@ -40,19 +60,78 @@ func (d *Digit) SetValue(value []digitValue) {
 // Draw draws this primitive onto the screen.
 func (d *Digit) Draw(screen tcell.Screen) {
 	d.Box.DrawForSubclass(screen, d)
-	x, y, _, _ := d.Box.GetInnerRect()
+	x, y, w, _ := d.Box.GetInnerRect()
+
 	for digit := range d.value {
 		for i, s := range text[d.value[digit]] {
-			fmt.Println(i)
-			screen.SetContent(x+digit*4, y+i, ' ', s, tcell.StyleDefault)
+			if x+(digit+1)*4 <= w {
+				tview.Print(screen, s, x+(digit)*4, i+y, w-x+(digit+1)*4-4, tview.AlignLeft, tcell.ColorDefault)
+			}
+		}
+	}
+}
+
+type Clock struct {
+	*Digit
+}
+
+func NewClock() *Clock {
+	digit := NewDigit()
+	digit.SetValue([]digitValue{Zero, Zero, Colon, Zero, Zero, Colon, Zero, Zero})
+	return &Clock{
+		Digit: digit,
+	}
+}
+
+func (m *Clock) Update() {
+	t := int(time.Now().Unix())
+	sec := (t + 5.5*3600) % 86400
+	min := sec / 60
+	sec %= 60
+	hour := min / 60
+	min %= 60
+
+	m.SetValue([]digitValue{
+		digitValue(hour / 10),
+		digitValue(hour % 10),
+		Colon,
+		digitValue(min / 10),
+		digitValue(min % 10),
+		Colon,
+		digitValue(sec / 10),
+		digitValue(sec % 10),
+	})
+}
+
+var (
+	app   *tview.Application
+	clock *Clock
+)
+
+func refresh() {
+	tick := time.NewTicker(100 * time.Millisecond)
+	for {
+		select {
+		case <-tick.C:
+			clock.Update()
+			app.Draw()
 		}
 	}
 }
 
 func main() {
-	box := NewDigit()
-	app := tview.NewApplication()
-	app.SetRoot(box, true)
+	clock = NewClock()
+	flex := tview.NewFlex().
+		AddItem(clock, 0, 1, false).
+		AddItem(tview.NewBox().SetBorder(true).SetTitle("Blink").SetBorderAttributes(tcell.AttrBlink), 0, 1, false).
+		AddItem(tview.NewBox().SetBorder(true).SetTitle("x"), 0, 1, false).
+		AddItem(tview.NewBox().SetBorder(true).SetTitle("x"), 0, 1, false).
+		AddItem(tview.NewBox().SetBorder(true).SetTitle("x"), 0, 1, false)
+
+	// box := NewDigit()
+	app = tview.NewApplication()
+	app.SetRoot(flex, true)
+	go refresh()
 	if err := app.Run(); err != nil {
 		panic(err)
 	}
