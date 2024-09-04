4bed73f80d8c1a36e68ecd324e390f1e3abb904e
Rohit Gupta <rohitrgupta@gmail.com>
2024-09-04 12:31:46 +0530

    log to redis

--- libs/017_logs/main.go
+++ libs/017_logs/main.go
@@ -2,22 +2,48 @@ package main
 
 import (
 	"bytes"
-	"fmt"
+	"context"
 	"log"
 	"log/slog"
+	"strings"
+
+	"github.com/redis/go-redis/v9"
 )
+
+var ctx = context.Background()
 
 func main() {
 	var (
 		buf bytes.Buffer
 	)
-	handler := slog.NewTextHandler(&buf, &slog.HandlerOptions{Level: slog.LevelDebug})
+	handler := slog.NewTextHandler(&buf, &slog.HandlerOptions{Level: slog.LevelDebug, AddSource: true})
 	logger := slog.New(handler)
 
 	log.SetFlags(log.Ldate | log.Lmicroseconds | log.Lshortfile)
 
 	logger.Info("Hello, log file!")
 	logger.Warn("hello", "count", 3)
+	logger.Debug("hello", "count", 3)
 
-	fmt.Print(&buf)
+	logger.Error("hello", "count", 3)
+	logger.WithGroup("decoder").Info("hello", "count", 3)
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       15, // use default DB
+	})
+
+	// values := make([]strinng, 0)
+	v := strings.Split(buf.String(), "\n")
+	// fmt.Println(v)
+	var v1 []interface{}
+	for _, v := range v {
+		v1 = append(v1, v)
+	}
+	err := rdb.RPush(ctx, "key1", v1...).Err()
+	if err != nil {
+		panic(err)
+	}
+	// fmt.Print(&buf)
 }
